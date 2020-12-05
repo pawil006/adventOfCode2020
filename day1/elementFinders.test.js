4bca5dc6861ce73082816742f3f01531a4b3a9f0
Paweł Zabczyński <zabczynskipawe@gmail.com>
2020-12-05 12:32:58 +0100

    - add unit tests instead of index file for dey1 solutions - improve cs - create commons allowing load test data from file

---
+++ day1/elementFinders.test.js
@@ -0,0 +1,29 @@
+const {loadArrayFrom} = require('../commons/testDataLoader');
+const {expect} = require('chai');
+const {solutionFind2, solutionFind3} = require('./elementsFinder')
+
+describe('for given data', () => {
+    const data = loadArrayFrom('./day1/input.txt');
+
+    describe('should find 2 numbers [...] and', () => {
+        it('should [...] calculate multiplication all of them for given target', () => {
+            // given
+            const target = 2020;
+            // when
+            const result = solutionFind2(data, target)
+            // then
+            expect(357504).to.eq(result)
+        })
+    })
+
+    describe('should find 3 numbers [...] and', () => {
+        it('should [...] calculate multiplication all of them for given target', () => {
+            // given
+            const target = 2020;
+            // when
+            const result = solutionFind3(data, target)
+            // then
+            expect(12747392).to.eq(result)
+        })
+    })
+})
